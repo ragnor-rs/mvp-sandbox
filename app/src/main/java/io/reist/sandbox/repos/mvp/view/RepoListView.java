5edf5394ad99a7ad1e4fa11b40bca64a45b643b2
defuera <denisv@bookmate.com>
2015-11-05 18:13:45 +0300

    move RepoListPresenter.View to  to mvp.views.RepoListView

---
+++ app/src/main/java/io/reist/sandbox/repos/mvp/view/RepoListView.java
@@ -0,0 +1,10 @@
+package io.reist.sandbox.repos.mvp.view;
+
+import io.reist.sandbox.core.mvp.view.BaseView;
+
+/**
+ * Created by defuera on 05/11/2015.
+ */
+public interface RepoListView extends BaseView {
+    void showLoader(boolean show);
+}
